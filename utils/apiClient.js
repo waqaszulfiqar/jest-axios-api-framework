fb68e4f462b9df80839c8382fca0d66cfe7fd056
waqaszulfiqar <waqas@bottleneck.com>
2025-01-03 18:14:11 +0500

    added comments

--- utils/apiClient.js
+++ utils/apiClient.js
@@ -1,20 +1,23 @@
 import axios from "axios";
 
 const apiClient = async (method, url, data = null, params = null) => {
+  // Configuration object for the axios request
   const config = {
     method: method,
-    url: `https://reqres.in/api${url}`,
+    url: `https://reqres.in/api${url}`, // Base URL with endpoint
     data: data,
     params: params,
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json", // Set content type to JSON
     },
   };
 
   try {
+    // Make the HTTP request using axios
     const response = await axios(config);
     return response; // Return the full response object
   } catch (error) {
+    // Throw the error response data or error message
     throw error.response ? error.response.data : error.message;
   }
 };
